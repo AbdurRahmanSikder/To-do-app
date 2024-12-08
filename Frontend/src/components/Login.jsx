42188169fe1422d897588495dbc0c53b8e04ad98
Abdur Rahman <119144605+AbdurRahmanSikder@users.noreply.github.com>
2024-12-08 10:29:53 +0600

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
     const handleRegister = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post("http://localhost:3000/user/login", {
+            const response = await axios.post("https://todo-app-6kxd.onrender.com/user/login", {
       
                 email,
                 password
